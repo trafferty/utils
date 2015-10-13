4fcef869643fe78b163d6a7d7a2d9ae2261f9c7e
Tom Rafferty <trafferty@cnt.canon.com>
2015-10-13 10:49:44 -0500

    added thread to read socket input

--- python/tcp_tester.py
+++ python/tcp_tester.py
@@ -1,8 +1,20 @@
 import socket
 import time
+import threading
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect(('127.0.0.1', 12070))
+
+def read_socket():
+    while True:
+        line = clientsocket.recv(1024)
+        print line,
+        time.sleep(.01)
+
+
+_thread = threading.Thread(target=read_socket)
+_thread.setDaemon(1)
+_thread.start()
 
 cmd_lst = ['{"method":"start"}\n', '{"method":"end"}\n']
 
@@ -12,5 +24,5 @@ for i in range(0, 20000):
     iter_cnt += 1
     print("Starting iter %d" % (iter_cnt))
     clientsocket.send(cmd_lst[cmd_num])
-    time.sleep(15)
+    time.sleep(8)
     cmd_num = cmd_num ^ 1
