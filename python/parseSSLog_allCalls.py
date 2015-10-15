ade729d930d94cc18a7cbe89df09e99bbf467b6b
Tom Rafferty <trafferty@cnt.canon.com>
2015-10-15 17:18:05 -0500

    added histogram analysis to log data

--- python/parseSSLog_allCalls.py
+++ python/parseSSLog_allCalls.py
@@ -6,16 +6,18 @@ import re
 import argparse
 import json
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import datetime as dt
+import collections
 
 # local:
 from colors import *
 
-#PrintColors = (BRIGHT_YELLOW, BRIGHT_CYAN)
+#PrintColors = (BRIGHT_WHITE, WHITE)
 PrintColors = (WHITE, WHITE)
 
-def parseSSLog(ss_log, output_path, generic=False):
+def parseSSLog(ss_log, output_path, pdf_path, generic=False):
     '''
     import re
     p = re.compile(ur'../../15\ (?P<start_ts>[0-9:.]*): \(XaarCmdAPI      \) \[DEBUG\] Calling (?P<func_name>[a-zA-Z]*).*?\n../../15\ (?P<end_ts>[0-9:.]*): \(XaarCmdAPI      \) \[DEBUG\] [Call(s)]* success!', re.DOTALL)
@@ -64,26 +66,97 @@ def parseSSLog(ss_log, output_path, generic=False):
         if func_name not in func_deltas:
             func_deltas[func_name] = []
         func_deltas[func_name].append(delta)
+    func_deltas_ordered = collections.OrderedDict(sorted(func_deltas.items()))
 
     func_metrics = {}
-    for k in func_deltas.keys():
-        deltas_np = np.array(func_deltas[k])
+    for k in func_deltas_ordered.keys():
+        deltas_np = np.array(func_deltas_ordered[k])
         func_metrics[k] = []
         func_metrics[k].append( len(deltas_np) )
         func_metrics[k].append( deltas_np.max() )
         func_metrics[k].append( deltas_np.min() )
         func_metrics[k].append( deltas_np.mean() )
+        func_metrics[k].append( deltas_np.std() )
+    func_metrics_ordered = collections.OrderedDict(sorted(func_metrics.items()))
 
-    print "%s---------------------------------------------------------------------------------------------------%s" % (WHITE, RESET)
-    print "%s Scorpion                                                     Sample       Max      Min      Mean  %s" % (WHITE, RESET)
-    print "%s Func name                                                     Count      (ms)     (ms)      (ms)  %s" % (WHITE, RESET)
-    print "%s---------------------------------------------------------------------------------------------------%s" % (WHITE, RESET)
+    print "%s---------------------------------------------------------------------------------------------------------------%s" % (BRIGHT_WHITE, RESET)
+    print "%s Scorpion                                                     Sample       Max      Min      Mean     Stdev    %s" % (BRIGHT_WHITE, RESET)
+    print "%s Func name                                                     Count      (ms)     (ms)      (ms)      (ms)    %s" % (BRIGHT_WHITE, RESET)
+    print "%s---------------------------------------------------------------------------------------------------------------%s" % (BRIGHT_WHITE, RESET)
     idx = 0
-    for k in func_metrics.keys():
-        print("%s%-57s  %9d   %7.1f  %7.1f   %7.1f %s" % 
-            (PrintColors[idx%2], k, func_metrics[k][0], func_metrics[k][1], func_metrics[k][2], func_metrics[k][3],
-             RESET))
+    for k in func_metrics_ordered.keys():
+        print("%s%-57s  %9d   %7.1f  %7.1f   %7.1f   %7.1f %s" % 
+            (PrintColors[idx%2], k, func_metrics_ordered[k][0], func_metrics_ordered[k][1], func_metrics_ordered[k][2], 
+             func_metrics_ordered[k][3], func_metrics_ordered[k][4], RESET))
         idx+=1
+    
+    print("\nNow creating histograms for each function, see %s" % pdf_path)
+
+    with PdfPages(pdf_path) as pdf:
+        d = pdf.infodict()
+        d['Title'] = u'ScorpionDLL v6.2 FunctionTiming'
+        d['Author'] = u'Tom H. Rafferty / CNT'
+        d['Subject'] = u'ScorpionDLL v6.2 FunctionTiming'
+        d['CreationDate'] = dt.datetime.today()
+        d['ModDate'] = dt.datetime.today()
+
+        bins = 100
+        for k in func_deltas_ordered.keys():
+            deltas_np = np.array(func_deltas_ordered[k])
+            max = deltas_np.max()
+            min = deltas_np.min()
+            if max > bins: 
+                binL = min
+                binH = max
+            else:
+                binL = 0
+                binH = bins
+
+            #fig = plt.figure(figsize=(10*2,5))
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_title('Histogram: %s' % k)
+            ax.set_ylabel('Bin Count')
+            ax.set_xlabel('Call Time (ms)')
+            ax.text(0.75, 0.75, "Call Stats:",
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+            ax.text(0.75, 0.70, ("n: %d" % (len(deltas_np))),
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+            ax.text(0.75, 0.65, ("min: %.2f" % (deltas_np.min())),
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+            ax.text(0.75, 0.60, ("max: %.2f" % (deltas_np.max())),
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+            ax.text(0.75, 0.55, ("mean: %.2f" % (np.round(deltas_np.mean()))),
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+            ax.text(0.75, 0.50, ("stdev: %.2f" % (deltas_np.std())),
+                horizontalalignment='left',verticalalignment='bottom',transform=ax.transAxes)
+
+            ax.hist(deltas_np, bins, [binL, binH])
+            # ax.axvline(np.round(deltas_np.mean()), linewidth=2, linestyle='--',  color='g')
+            # ax.text(np.round(deltas_np.mean()), 0, "Mean",
+            #     horizontalalignment='center',verticalalignment='top')
+
+            # ax.axvline(np.round(deltas_np.max()), linewidth=2, linestyle='--',  color='r')
+            # ax.text(np.round(deltas_np.max()), 0, "Max",
+            #     horizontalalignment='center',verticalalignment='top')
+
+            # ax.axvline(np.round(deltas_np.min()), linewidth=2, linestyle='--',  color='c')
+            # ax.text(np.round(deltas_np.min()), 0, "Min",
+            #     horizontalalignment='center',verticalalignment='top')
+
+            # #hist,bins = np.histogram(deltas_np, 50, [0,50])
+            # plt.hist(deltas_np,bins,[0,bins])
+            # plt.ylim([ 0, max + 2 ])
+
+            # plt.text(bins*.7, max*.8, ("min=%.f" % (np.round(deltas_np.min()))), fontsize=14)
+            # plt.text(bins*.7, max*.8, ("max=%.f" % (np.round(deltas_np.max()))), fontsize=14)
+            # plt.text(bins*.7, max*.6, ("mean=%.f" % (np.round(deltas_np.mean()))), fontsize=14)
+            # plt.text(bins*.7, max*.5, ("stdev=%.f" % (np.round(deltas_np.std()))), fontsize=14)
+            # #plt.bar(hist)
+            # #plt.xlim([ bins[0], bins[-1] ])
+            # plt.title('Histogram: %s' % k)
+            pdf.savefig()  # saves the current figure into a pdf page
+            plt.close()
 
 if __name__ == "__main__":
     '''
@@ -94,11 +167,13 @@ if __name__ == "__main__":
                         help='input file...if not specified then use stdin')
     parser.add_argument('-o', '--output_path', dest='output_path', type=str,
                         help='output path...if not specified then will use /tmp', default='/tmp')
+    parser.add_argument('-p', '--pdf_path', dest='pdf_path', type=str,
+                        help='pdf path', default='/home/trafferty/ScorpionDLL_FuncCallTiming_Hists.pdf')
 
     args = parser.parse_args()
 
     if args.in_file:
-        parseSSLog(args.in_file, args.output_path)
+        parseSSLog(args.in_file, args.output_path, args.pdf_path)
     else:
         parser.print_help()
         sys.exit(1)
