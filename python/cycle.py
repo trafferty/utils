91ed3705b4c81a9eef1a92bf16c20d9b65042867
Tom Rafferty <thomasrafferty@gmail.com>
2016-09-02 22:23:17 +0000

    added loop sequence to cycle

--- python/cycle.py
+++ python/cycle.py
@@ -4,18 +4,23 @@ import time
 import argparse
 import CHIP_IO.GPIO as GPIO
 
-def cycle(n, f):
+def cycle(l, n, f, w):
     t = (1.0/f)
-    print("Starting cycle.  f=%.2f, t=%f, n=%d" % (f, t, n))
+    wait_adjusted = w - (n * t)
+    print("Starting, %d loops.  f=%.2f, t=%f, n=%d, wait=%f, wait_adjusted=%f" % (l, f, t, n, w, wait_adjusted))
     GPIO.output("CSID0", 0)
-    for x in range(n+1):
-        GPIO.output("CSID0", 1)
-        GPIO.output("CSID1", 1)
-        time.sleep(t/2.0)
-        GPIO.output("CSID0", 0)
-        GPIO.output("CSID1", 0)
-        time.sleep(t/2.0)
-        print("...cycle complete: %d" % (x))
+    GPIO.output("CSID1", 0)
+    for i in range(l):
+        for x in range(n):
+            GPIO.output("CSID0", 1)
+            GPIO.output("CSID1", 1)
+            time.sleep(t/2.0)
+            GPIO.output("CSID0", 0)
+            GPIO.output("CSID1", 0)
+            time.sleep(t/2.0)
+            print("  ...cycle complete: %d" % (x))
+        print("...loop complete: %d" % (i))
+        time.sleep(wait_adjusted)
 
 if __name__ == "__main__":
     '''
@@ -23,10 +28,12 @@ if __name__ == "__main__":
     '''
     parser = argparse.ArgumentParser(description='open process log file, parse it according to parse function')
     parser.add_argument('-n',  dest='num_cycles', type=int, default=10, help='number of cycles')
+    parser.add_argument('-l',  dest='loops', type=int, default=1, help='loops')
     parser.add_argument('-f',  dest='freq', type=float, default=1.0, help='frequency in Hz')
+    parser.add_argument('-w',  dest='wait', type=float, default=1.0, help='wait time between loops')
 
     args = parser.parse_args()
 
     GPIO.setup("CSID0", GPIO.OUT)
     GPIO.setup("CSID1", GPIO.OUT)
-    cycle(args.num_cycles, args.freq)
+    cycle(args.loops, args.num_cycles, args.freq, args.wait)
