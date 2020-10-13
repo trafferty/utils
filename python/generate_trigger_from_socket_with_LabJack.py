6c9fe82757b9924f6b17c6961d2217bb90c0d526
Tom Rafferty <trafferty@cnt.canon.com>
2020-10-13 09:40:36 -0500

    logging cleanup

--- python/generate_trigger_from_socket_with_LabJack.py
+++ python/generate_trigger_from_socket_with_LabJack.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     done = False
     def sigint_handler(signal, frame):
         global done
-        print( "\nShutting down...")
+        doLog( "\nShutting down...")
         done = True
     signal.signal(signal.SIGINT, sigint_handler)
     
@@ -34,19 +34,19 @@ if __name__ == "__main__":
 
     handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
     if handle is None:
-        logger.error("Could not connect to LabJack. Connected?")
+        doLog("Could not connect to LabJack. Connected?")
         sys.exit()
 
     info = ljm.getHandleInfo(handle)
-    print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+    doLog("Opened a LabJack with Device type: %i, Connection type: %i,\n"
           "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
           (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
     deviceType = info[0]
 
     # Setting FIO4 and 05 on the LabJack T4. FIO0-FIO3 are reserved for AIN0-AIN3.
-    trigger_name = "FIO4"
-    LED_name = "FIO5"
+    trigger_name = "FIO5"
+    LED_name = "FIO4"
 
     # Reading from the digital line in case it was previously an analog input.
     ljm.eReadName(handle, trigger_name)
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     # Start the server...
     server.start();
 
-    print("server started on %s:%d.\nstarting process loop..." % (addr, port))
+    doLog("Server started on %s:%d.\nStarting process loop..." % (addr, port))
     cnt = 0
     prev_ts = time.time()
     while not done:
@@ -80,9 +80,9 @@ if __name__ == "__main__":
                 break
         else:
             time.sleep(0.001)
-    print("rec'd quit cmd, stopping loop...")
+    doLog("rec'd quit cmd, stopping loop...")
 
     server.stop()
-    print("all shut down...")
+    doLog("all shut down...")
 
     ljm.close(handle)
